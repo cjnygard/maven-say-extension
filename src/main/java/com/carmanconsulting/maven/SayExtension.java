932a526d39aa2b701c37e67c85acef0694bb6b9a
James Carman <jcarman@apache.org>
2015-03-28 13:52:31 -0400

    Removing unnecessary "requirements"

--- src/main/java/com/carmanconsulting/maven/SayExtension.java
+++ src/main/java/com/carmanconsulting/maven/SayExtension.java
@@ -6,25 +6,12 @@ import org.apache.maven.MavenExecutionException;
 import org.apache.maven.execution.MavenExecutionResult;
 import org.apache.maven.execution.MavenSession;
 import org.apache.maven.project.MavenProject;
-import org.apache.maven.rtinfo.RuntimeInformation;
 import org.codehaus.plexus.component.annotations.Component;
-import org.codehaus.plexus.component.annotations.Requirement;
-import org.codehaus.plexus.logging.Logger;
 
 import java.io.IOException;
 
 @Component(role = AbstractMavenLifecycleParticipant.class)
 public class SayExtension extends AbstractMavenLifecycleParticipant {
-//----------------------------------------------------------------------------------------------------------------------
-// Fields
-//----------------------------------------------------------------------------------------------------------------------
-
-    @Requirement
-    private Logger logger;
-
-    @Requirement
-    private RuntimeInformation runtime;
-
 //----------------------------------------------------------------------------------------------------------------------
 // Other Methods
 //----------------------------------------------------------------------------------------------------------------------
@@ -48,6 +35,5 @@ public class SayExtension extends AbstractMavenLifecycleParticipant {
                 }
             }
         }
-
     }
 }
